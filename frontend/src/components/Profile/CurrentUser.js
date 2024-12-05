cba88dc84c8073170f0409a71ff962898de2e21f
dushyanth_099 <dushyanth0699@gmail.com>
2024-12-05 18:22:33 +0530

    updated

--- frontend/src/components/Profile/CurrentUser.js
+++ frontend/src/components/Profile/CurrentUser.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./CurrentUser.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const CurrentUser = () => {
   const [userDetails, setUserDetails] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const fetchUserDetails = async () => {
     try {
@@ -51,82 +52,98 @@ const CurrentUser = () => {
   }
 
   return (
-    <div className="container mt-4">
-      <div className="card shadow">
-        <div className="card-header text-center bg-primary text-white">
-          <h2>Hi, {userDetails?.name}</h2>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/LandingPage">
+            Tiruppur Matrimony
+          </a>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <button
+                className="btn btn-dark mx-2"
+                onClick={() =>navigate("/LandingPage")}
+              >
+                HomePage
+              </button>
+            </li>
+          </ul>
         </div>
-        <div className="card-body">
-          {userDetails ? (
-            <div className="row">
-              <div className="col-md-6 mb-3">
-                {userDetails.photo && (
-                  <img
-                    src={`http://localhost:5000/${userDetails.photo.replace(
-                      /\\/g,
-                      "/"
-                    )}`}
-                    alt=""
-                    style={{ height: "200px", objectFit: "cover" }}
-                  />
-                )}
-                <p>
-                  <strong>Name:</strong> {userDetails.name}
-                </p>
-                <p>
-                  <strong>Gender:</strong> {userDetails.gender}
-                </p>
-                <p>
-                  <strong>BirthDate:</strong> {userDetails.birthDate}
-                </p>
-                <p>
-                  <strong>Religion:</strong> {userDetails.religion}
-                </p>
-                <p>
-                  <strong>Mother Tongue:</strong> {userDetails.motherTongue}
-                </p>
+      </nav>
+      <div className="container mt-4">
+        <div className="card shadow">
+          <div className="card-header text-center bg-primary text-white">
+            <h2>Hi, {userDetails?.name}</h2>
+          </div>
+          <div className="card-body">
+            {userDetails ? (
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                  {userDetails.photo && (
+                    <img
+                      src={`http://localhost:5000/${userDetails.photo.replace(
+                        /\\/g,
+                        "/"
+                      )}`}
+                      alt=""
+                      style={{ height: "200px", objectFit: "cover" }}
+                    />
+                  )}
+                  <p>
+                    <strong>Name:</strong> {userDetails.name}
+                  </p>
+                  <p>
+                    <strong>Gender:</strong> {userDetails.gender}
+                  </p>
+                  <p>
+                    <strong>BirthDate:</strong> {userDetails.birthDate}
+                  </p>
+                  <p>
+                    <strong>Religion:</strong> {userDetails.religion}
+                  </p>
+                  <p>
+                    <strong>Mother Tongue:</strong> {userDetails.motherTongue}
+                  </p>
+                </div>
+                <div className="col-md-6 mb-3">
+                  <p>
+                    <strong>Height:</strong> {userDetails.height}
+                  </p>
+                  <p>
+                    <strong>Caste:</strong> {userDetails.caste}
+                  </p>
+                  <p>
+                    <strong>Sub Caste:</strong> {userDetails.subCaste}
+                  </p>
+                  <p>
+                    <strong>Gothram:</strong> {userDetails.gothram}
+                  </p>
+                  <p>
+                    <strong>Education:</strong> {userDetails.highestEducation}
+                  </p>
+                </div>
+                <div className="col-md-6 mb-3">
+                  <p>
+                    <strong>Occupation:</strong> {userDetails.occupation}
+                  </p>
+                  <p>
+                    <strong>Annual Income:</strong> {userDetails.annualIncome}
+                  </p>
+                  <p>
+                    <strong>State:</strong> {userDetails.state}
+                  </p>
+                  <p>
+                    <strong>About:</strong> {userDetails.about}
+                  </p>
+                </div>
               </div>
-              <div className="col-md-6 mb-3">
-                <p>
-                  <strong>Height:</strong> {userDetails.height}
-                </p>
-                <p>
-                  <strong>Caste:</strong> {userDetails.caste}
-                </p>
-                <p>
-                  <strong>Sub Caste:</strong> {userDetails.subCaste}
-                </p>
-                <p>
-                  <strong>Gothram:</strong> {userDetails.gothram}
-                </p>
-                <p>
-                  <strong>Education:</strong> {userDetails.highestEducation}
-                </p>
-              </div>
-              <div className="col-md-6 mb-3">
-                <p>
-                  <strong>Occupation:</strong> {userDetails.occupation}
-                </p>
-                <p>
-                  <strong>Annual Income:</strong> {userDetails.annualIncome}
-                </p>
-                <p>
-                  <strong>State:</strong> {userDetails.state}
-                </p>
-                <p>
-                  <strong>About:</strong> {userDetails.about}
-                </p>
-              </div>
-            </div>
-          ) : (
-            <p>No user details available</p>
-          )}
+            ) : (
+              <p>No user details available</p>
+            )}
+          </div>
         </div>
       </div>
-      <Link className="btn btn-warning btn-block" to="/LandingPage">
-        Homepage
-      </Link>
-    </div>
+    </>
   );
 };
 
--- frontend/src/components/Profile/UpdateProfile.js
+++ frontend/src/components/Profile/UpdateProfile.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./UpdateProfile.css";
-import { Link } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
-
+import { useNavigate } from "react-router-dom";
 const UpdateProfile = () => {
   const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const fetchUserDetails = async () => {
     const token = localStorage.getItem("token");
@@ -98,231 +98,247 @@ const UpdateProfile = () => {
   }, []);
 
   return (
-    <div className="container mt-5 update-profile">
-      <h2 className="text-center mb-4">Update Profile</h2>
-      {error && <p className="alert alert-danger">{error}</p>}
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          updateUserDetails();
-        }}
-      >
-        <div className="row">
-          <div className="col-md-6 mb-3">
-            <label>Name</label>
-            <input
-              type="text"
-              name="name"
-              className="form-control"
-              value={formData.name}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>Email</label>
-            <input
-              type="email"
-              name="email"
-              className="form-control"
-              value={formData.email}
-              onChange={handleInputChange}
-            />
-          </div>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/LandingPage">
+            Tiruppur Matrimony
+          </a>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <button
+                className="btn btn-dark mx-2"
+                onClick={() => navigate("/LandingPage")}
+              >
+                HomePage
+              </button>
+            </li>
+          </ul>
         </div>
-
-        {/* Add other form fields in similar row structures */}
-        <div className="row">
-          <div className="col-md-6 mb-3">
-            <label>Gender</label>
-            <select
-              name="gender"
-              className="form-control"
-              value={formData.gender}
-              onChange={handleInputChange}
-            >
-              <option value="">Select Gender</option>
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-            </select>
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>BirthDate</label>
-            <input
-              type="date"
-              name="birthDate"
-              className="form-control"
-              value={formData.birthDate}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>religion</label>
-            <input
-              type="text"
-              name="religion"
-              className="form-control"
-              value={formData.religion}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>motherTongue</label>
-            <input
-              type="text"
-              name="motherTongue"
-              className="form-control"
-              value={formData.motherTongue}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>caste</label>
-            <input
-              type="text"
-              name="caste"
-              className="form-control"
-              value={formData.caste}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>subCaste</label>
-            <input
-              type="text"
-              name="subcaste"
-              className="form-control"
-              value={formData.subCaste}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>gothram</label>
-            <input
-              type="text"
-              name="gothram"
-              className="form-control"
-              value={formData.gothram}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>height</label>
-            <input
-              type="number"
-              name="height"
-              className="form-control"
-              value={formData.height}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>highestEducation</label>
-            <input
-              type="text"
-              name="highestEducation"
-              className="form-control"
-              value={formData.highestEducation}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>employedIn</label>
-            <input
-              type="text"
-              name="employedIn"
-              className="form-control"
-              value={formData.employedIn}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>occupation</label>
-            <input
-              type="text"
-              name="occupation"
-              className="form-control"
-              value={formData.occupation}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>annualIncome</label>
-            <input
-              type="number"
-              name="annualIncome"
-              className="form-control"
-              value={formData.annualIncome}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>workLocation</label>
-            <input
-              type="text"
-              name="workLocation"
-              className="form-control"
-              value={formData.workLocation}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>state</label>
-            <input
-              type="text"
-              name="state"
-              className="form-control"
-              value={formData.state}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>city</label>
-            <input
-              type="text"
-              name="city"
-              className="form-control"
-              value={formData.city}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label>about</label>
-            <input
-              type="text"
-              name="about"
-              className="form-control"
-              value={formData.about}
-              onChange={handleInputChange}
-            />
-          </div>
-        </div>
-
-        {/* File Upload */}
-        <div className="row">
-          <div className="col-md-6 mb-3">
-            <label>Photo</label>
-            <input
-              type="file"
-              name="photo"
-              className="form-control"
-              onChange={handleFileChange}
-            />
-          </div>
-        </div>
-
-        <button
-          type="submit"
-          className="btn btn-success btn-block"
-          disabled={loading}
+      </nav>
+      <div className="container mt-5 update-profile">
+        <h2 className="text-center mb-4">Update Profile</h2>
+        {error && <p className="alert alert-danger">{error}</p>}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            updateUserDetails();
+          }}
         >
-          {loading ? "Updating..." : "Update Profile"}
-        </button>
-        <ToastContainer position="top-right" autoClose={3000} />
-        <Link className="btn btn-warning btn-block" to="/LandingPage">
-          Homepage
-        </Link>
-      </form>
-    </div>
+          <div className="row">
+            <div className="col-md-6 mb-3">
+              <label>Name</label>
+              <input
+                type="text"
+                name="name"
+                className="form-control"
+                value={formData.name}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>Email</label>
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+
+          {/* Add other form fields in similar row structures */}
+          <div className="row">
+            <div className="col-md-6 mb-3">
+              <label>Gender</label>
+              <select
+                name="gender"
+                className="form-control"
+                value={formData.gender}
+                onChange={handleInputChange}
+              >
+                <option value="">Select Gender</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>BirthDate</label>
+              <input
+                type="date"
+                name="birthDate"
+                className="form-control"
+                value={formData.birthDate}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>religion</label>
+              <input
+                type="text"
+                name="religion"
+                className="form-control"
+                value={formData.religion}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>motherTongue</label>
+              <input
+                type="text"
+                name="motherTongue"
+                className="form-control"
+                value={formData.motherTongue}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>caste</label>
+              <input
+                type="text"
+                name="caste"
+                className="form-control"
+                value={formData.caste}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>subCaste</label>
+              <input
+                type="text"
+                name="subcaste"
+                className="form-control"
+                value={formData.subCaste}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>gothram</label>
+              <input
+                type="text"
+                name="gothram"
+                className="form-control"
+                value={formData.gothram}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>height</label>
+              <input
+                type="number"
+                name="height"
+                className="form-control"
+                value={formData.height}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>highestEducation</label>
+              <input
+                type="text"
+                name="highestEducation"
+                className="form-control"
+                value={formData.highestEducation}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>employedIn</label>
+              <input
+                type="text"
+                name="employedIn"
+                className="form-control"
+                value={formData.employedIn}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>occupation</label>
+              <input
+                type="text"
+                name="occupation"
+                className="form-control"
+                value={formData.occupation}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>annualIncome</label>
+              <input
+                type="number"
+                name="annualIncome"
+                className="form-control"
+                value={formData.annualIncome}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>workLocation</label>
+              <input
+                type="text"
+                name="workLocation"
+                className="form-control"
+                value={formData.workLocation}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>state</label>
+              <input
+                type="text"
+                name="state"
+                className="form-control"
+                value={formData.state}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>city</label>
+              <input
+                type="text"
+                name="city"
+                className="form-control"
+                value={formData.city}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="col-md-6 mb-3">
+              <label>about</label>
+              <input
+                type="text"
+                name="about"
+                className="form-control"
+                value={formData.about}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+
+          {/* File Upload */}
+          <div className="row">
+            <div className="col-md-6 mb-3">
+              <label>Photo</label>
+              <input
+                type="file"
+                name="photo"
+                className="form-control"
+                onChange={handleFileChange}
+              />
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-success btn-block"
+            disabled={loading}
+          >
+            {loading ? "Updating..." : "Update Profile"}
+          </button>
+          <ToastContainer position="top-right" autoClose={3000} />
+        </form>
+      </div>
+    </>
   );
 };
 
